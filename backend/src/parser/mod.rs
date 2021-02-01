1b8dc03eea54ac4808558cfc4390322b727e25ac
Sebastián Aedo <sebastian.aedo@sansano.usm.cl>
2021-02-01 00:22:37 -0300

    Barebones UI
    Added interface to use Rust's FFI with C#. Json is arriving just fine.
    Added very basic C# GUI App.

--- backend/src/parser/mod.rs
+++ backend/src/parser/mod.rs
@@ -1,151 +1,45 @@
+pub mod definitions;
+
+use std::os::raw::c_char;
+use std::ffi::{CStr, CString};
+use std::path::Path;
 use deku::prelude::*;
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 
-#[derive(DekuRead, DekuWrite, Debug, Serialize)]
-#[deku(magic = b"YZFH")]
-pub struct SaveFile {
-    #[deku(pad_bytes_before = "0x24")]
-    pub time: SaveTime,
+#[no_mangle]
+pub extern "system" fn parse_file(name: *const c_char) -> *const c_char {
+    assert!(!name.is_null());
+    let received = unsafe { CStr::from_ptr(name) };
+    let mut path_string = {
+        let s = received.to_str().unwrap();
+        String::from(s)
+    };
 
-    #[deku(pad_bytes_before = "0x40F")]
-    pub difficulty: Difficulty,
+    let path = Path::new(&path_string);
+    if !path.is_file() {
+        println!("path wasn't a file");
+        return std::ptr::null();
+    }
 
-    #[deku(pad_bytes_before = "0x86")]
-    pub current_char: CurrentCharacter,
+    let mut file = std::fs::read(path).unwrap();
 
-    // TODO: Write an appropiate parser for the character skills
-    #[deku(pad_bytes_before = "0x1A9F")]
-    pub skills_brawler: u64,
-    #[deku(pad_bytes_before = "0x18")]
-    pub skills_rush: u64,
-    #[deku(pad_bytes_before = "0x18")]
-    pub skills_beast: u64,
-    #[deku(pad_bytes_before = "0x18")]
-    pub skills_thug: u64,
-    #[deku(pad_bytes_before = "0x18")]
-    pub skills_slugger: u64,
-    #[deku(pad_bytes_before = "0x18")]
-    pub skills_breaker: u64,
+    let (_rest, mut save) = definitions::SaveFile::from_bytes((file.as_ref(), 0)).unwrap();
 
-    #[deku(pad_bytes_before = "0x58", bits = "2")]
-    pub _unused: u8,
+    let serialized = serde_json::to_string(&save).unwrap();
 
-    #[deku(bits = "1")]
-    pub style_mdos: bool,
-
-    #[deku(bits = "1")]
-    pub style_breaker: bool,
-
-    #[deku(bits = "1")]
-    pub style_slugger: bool,
-
-    #[deku(bits = "1")]
-    pub style_dod: bool,
-
-    #[deku(bits = "1")]
-    pub style_beast: bool,
-
-    #[deku(bits = "1")]
-    pub style_rush: bool,
-
-    #[deku(pad_bytes_before = "0xd3f")]
-    pub outfit: Outfit,
-
-    #[deku(pad_bytes_before = "0x4106")]
-    pub map_id: u8,
-
-    #[deku(pad_bytes_before = "0xE3")]
-    pub coord_x: f32,
-    pub coord_y: f32,
-    pub coord_z: f32,
-
-    #[deku(pad_bytes_before = "0xB08", count = "20")]
-    pub kiryu_items: Vec<Item>,
-
-    #[deku(count = "20")]
-    pub majima_items: Vec<Item>,
-
-    #[deku(pad_bytes_before = "0x20", count = "20")]
-    pub kiryu_valuables: Vec<Item>,
-
-    #[deku(pad_bytes_before = "0x51C0", count = "15")]
-    pub kiryu_weapons: Vec<Item>,
-
-    #[deku(pad_bytes_before = "0x2184")]
-    pub kiryu_money: u64,
-    pub majima_money: u64,
-
-    #[deku(pad_bytes_before = "0x110")]
-    pub kiryu_cp: u32,
-
-    #[deku(pad_bytes_before = "0x4")]
-    pub majima_cp: u32,
-
-    #[deku(pad_bytes_before = "0x1D34")]
-    pub kamurocho_pool: u64,
-
-    #[deku(pad_bytes_before = "0x28")]
-    pub sotenbori_pool: u64,
-
+    // Convert it to CString at the end
+    let result = CString::new(serialized).unwrap();
+    let p = result.as_ptr();
+    std::mem::forget(result);
+    p
 }
 
-#[derive(DekuRead, DekuWrite, Debug, Serialize)]
-pub struct SaveTime {
-    year: u16,
-    month: u16,
-    day: u16,
-    hour: u16,
-    minute: u16,
-    second: u16,
-    milli: u16
-}
-
-#[derive(DekuRead, DekuWrite, Debug, Serialize)]
-#[deku(type = "u8")]
-pub enum Difficulty {
-    Easy = 0x0,
-    Normal,
-    Hard,
-    Extreme
-}
-
-#[derive(DekuRead, DekuWrite, Debug, Serialize)]
-#[deku(type = "u8")]
-pub enum CurrentCharacter {
-    Kiryu = 0x1,
-    Majima
-}
-
-#[derive(DekuRead, DekuWrite, Debug, Serialize)]
-pub struct Outfit {
-    #[deku(bits = "1")]
-    pub dod: bool,
-    #[deku(bits = "1")]
-    pub hanya: bool,
-    #[deku(bits = "1")]
-    pub dragon: bool,
-    #[deku(bits = "1")]
-    pub mad_dog: bool,
-    #[deku(bits = "1")]
-    pub judgement: bool,
-    #[deku(bits = "1")]
-    pub cinderella: bool,
-    #[deku(bits = "1")]
-    pub producer: bool,
-    #[deku(bits = "1")]
-    pub parka: bool,
-    #[deku(bits = "1")]
-    pub lotn: bool,
-    #[deku(bits = "1")]
-    pub new_hire: bool,
-
-    #[deku(bits = "6")]
-    _unused: u8
-}
-
-#[derive(DekuRead, DekuWrite, Debug, Serialize)]
-pub struct Item {
-    id: u16,
-    #[deku(pad_bytes_before = "0x4", pad_bytes_after = "0x8")]
-    uses: u16
+#[no_mangle]
+pub unsafe extern "system" fn free_string(s: *mut c_char) {
+    if s.is_null() {
+        println!("Pointer was null");
+        return;
+    }
+    println!("Freeing pointer");
+    CString::from_raw(s);
 }
---
+++ frontend/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace frontend
+{
+    
+    static class Program
+    {
+
+        [DllImport("backend.dll", EntryPoint = "parse_file")]
+        public static extern IntPtr parse_file([MarshalAs(UnmanagedType.LPStr)] string t);
+        [DllImport("backend.dll", EntryPoint = "free_string")]
+        public static extern void free_string(IntPtr t);
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string test = "..\\backend\\tests\\save_test.sav";
+            IntPtr t = parse_file(test);
+
+            string parsed = Marshal.PtrToStringAnsi(t);
+
+            free_string(t);
+
+            System.Diagnostics.Debug.WriteLine($"{test} {parsed}");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
